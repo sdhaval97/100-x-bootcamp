5510c5b25ca14de59b7c6433b91c9b2d5653aff1
sdhaval97 <sdhaval97@gmail.com>
2026-01-09 01:06:12 +0530

    practiced a bit of js

---
+++ js-warmup/script.js
@@ -0,0 +1,11 @@
+let name = prompt("What is your name?");
+alert(`Your name is ${name}`);
+
+function min(a, b) {
+    if (a<b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log(min(4, 5));
